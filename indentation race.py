9dc7bc02e19dd5abb4699317ec2cf9f289658fa9
Nalule Esther Precious <estherprecious473@gmail.com>
2025-06-26 19:31:21 +0300

    Add files via upload

---
+++ indentation race.py
@@ -0,0 +1,28 @@
+def main():
+    print("Welcome to the Indentation Race!")
+for i in range(5):
+    print("Current number:", i)
+    if i % 2 == 0:
+       print("Even number.")
+    else:
+        print("Odd number.") 
+          
+        print("Race completed!")  
+
+
+
+def calculate_total_price(item_price, quantity):
+    item_total = item_price * quantity
+    tax_rate = 0.07
+    tax_amount = item_total * tax_rate
+    total_price = item_total + tax_amount
+    
+    discount = 0.1
+    discounted_price = total_price - (total_price * discount)
+
+    return discounted_price
+
+item_price = 25.0
+quantity = 4
+
+print("Total Price:", calculate_total_price(item_price, quantity))
